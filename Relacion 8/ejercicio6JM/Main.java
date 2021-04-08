be21fd242880268d206963654381e650f8639ade
Adela Lira <aalira.96@gmail.com>
2021-04-08 08:48:11 +0200

    Boletin 8, Ejercicio 6 realizado por JM

---
+++ Relacion 8/ejercicio6JM/Main.java
@@ -0,0 +1,15 @@
+package ejercicio6JM;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Frase frase = new Frase();
+
+		
+		frase.contarOcurrenciasFrase("El ordenador con procesador pentium iv");
+	
+		System.out.println(frase.mostrarOcurrencias());
+	}
+
+}
